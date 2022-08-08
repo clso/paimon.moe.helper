5118d3957b0fdf3de8194a49aec34463943a50a9
clso <694743+clso@users.noreply.github.com>
2022-08-08 13:50:54 +0800

    Create main.vb

---
+++ main.vb
@@ -0,0 +1,47 @@
+' paimon.moe import helper
+' https://paimon.moe/wish/import
+
+Imports System.IO
+Imports System.Text
+Imports System.Text.RegularExpressions
+Imports System.Windows
+
+Module paimon_moe_helper
+
+	Sub Main(ByVal pars As String())
+		Dim args As New List(Of String)(pars)
+		Dim logPath As String = "%userprofile%\AppData\LocalLow\miHoYo\Genshin Impact\output_log.txt"
+		If args.Contains("-CN") Then
+			logPath = "%userprofile%\AppData\LocalLow\miHoYo\原神\output_log.txt"
+		End If
+		logPath = Environment.ExpandEnvironmentVariables(logPath)
+
+		If Not File.Exists(logPath) Then
+			Console.WriteLine("Cannot find the log file! Make sure to open the wish history first!")
+			Console.ReadLine()
+			Return
+		End If
+
+		Dim fs = New FileStream(logPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)
+		Dim sr As New StreamReader(fs, Encoding.UTF8)
+		Dim log As String = sr.ReadToEnd()
+		sr.Close()
+
+		Dim m As Match = Regex.Match(log, "^OnGetWebViewPageFinish:(.+)$", RegexOptions.IgnoreCase Or RegexOptions.Multiline)
+		If m.Success Then
+			Console.WriteLine("Copy to Clipboard: " & m.Groups(1).Value)
+			' ref assembly PresentationCore.dll
+			Clipboard.SetDataObject(m.Groups(1).Value)
+
+			Beep()
+			'Console.Beep()
+			'Console.ReadLine()
+		Else
+			Console.WriteLine("Cannot find the log file! Make sure to open the wish history first!")
+			Console.ReadLine()
+		End If
+
+
+	End Sub
+
+End Module
