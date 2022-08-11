1b482be7eaab7f6f0cd86125bcf07dcebd68ed60
clso <694743+clso@users.noreply.github.com>
2022-08-11 15:43:05 +0800

    Update main.cs

--- main.cs
+++ main.cs
@@ -6,7 +6,7 @@ using System.IO;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
-namespace ConsoleApplication1
+namespace paimon_moe_helper
 {
 	class Program
 	{
@@ -15,7 +15,7 @@ namespace ConsoleApplication1
 		{
 			List<string> args = new List<string>(pars);
 			string logPath = "%userprofile%\\AppData\\LocalLow\\miHoYo\\Genshin Impact\\output_log.txt";
-			if( args.Contains("-CN") ) {
+			if( args.Contains("-CN", StringComparer.OrdinalIgnoreCase) ) {
 				logPath = "%userprofile%\\AppData\\LocalLow\\miHoYo\\原神\\output_log.txt";
 			}
 			logPath = Environment.ExpandEnvironmentVariables(logPath);
@@ -26,33 +26,27 @@ namespace ConsoleApplication1
 				return;
 			}
 
-			FileStream fs = new FileStream(logPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-			StreamReader sr = new StreamReader(fs, Encoding.UTF8);
-			string log = sr.ReadToEnd();
-			sr.Close();
+			string log;
+			using( FileStream fs = new FileStream(logPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite) ) {
+				log = new StreamReader(fs, Encoding.UTF8).ReadToEnd();
+			}
 
 			MatchCollection mc = Regex.Matches(log, "^OnGetWebViewPageFinish:(.+)$", RegexOptions.IgnoreCase | RegexOptions.Multiline);
 			Match m;
 			if( mc.Count > 0 ) {
 				m = mc[mc.Count - 1];
-			} else {
-				Console.WriteLine("Cannot find the log file! Make sure to open the wish history first!");
-				Console.ReadLine();
-				return;
-			}
-			
-			if( m.Success ) {
+
 				Console.WriteLine("Copy to Clipboard: " + m.Groups[1].Value);
 				// ref assembly System.Windows.Forms.dll
 				Clipboard.SetDataObject(m.Groups[1].Value, true);
 
 				System.Media.SystemSounds.Beep.Play();
-				//Console.Beep();
 				//Console.ReadLine();
 			} else {
 				Console.WriteLine("Cannot find the log file! Make sure to open the wish history first!");
 				Console.ReadLine();
 			}
+
 		}
 	}
 }
